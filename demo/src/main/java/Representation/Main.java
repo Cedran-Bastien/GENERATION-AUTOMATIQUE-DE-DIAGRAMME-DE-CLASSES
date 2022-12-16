2615e1a2c72d51ab787843a9a941e70cdbea4218
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2022-12-16 12:06:54 +0100

    test modele

---
+++ demo/src/main/java/Representation/Main.java
@@ -0,0 +1,8 @@
+package Representation;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Modele m =new Modele("src/");
+        m.toString();
+    }
+}
