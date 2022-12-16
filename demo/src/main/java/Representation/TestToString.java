2c44d8d2597b921df1efcc1248f2c50af23d8834
DIDIer5454 <menene12.akoo@gmail.com>
2022-12-16 11:55:34 +0100

    TestClasse

--- demo/src/main/java/Representation/TestToString.java
+++ demo/src/main/java/Representation/TestToString.java
@@ -5,16 +5,34 @@ import org.testng.annotations.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestToString {
-    @Test
-    public void testMethod(){
-        Methode methode=new Methode("testeur","int");
-            assertEquals(methode.toString(),"public testeur():int");
+    private interface inter {
+        int n = 3;
+
+        public int getn();
     }
+
     @Test
-    public void testAttribut(){
-Attribut attribut=new Attribut("testeur","int");
-assertEquals(attribut.toString(),"public testeur int");
+    public void testMethod() {
+        Methode methode = new Methode("testeur", "int");
+        assertEquals(methode.toString(), "public testeur():int");
     }
+
     @Test
-    public void
+    public void testAttribut() {
+        Attribut attribut = new Attribut("testeur", "int");
+        assertEquals(attribut.toString(), "public testeur int");
+    }
+
+    @Test
+    public void testInterface() {
+        Interface i = new Interface(inter.class);
+        String verify = "private I inter\n" +
+                "public final static int n\n" +
+                "public getn()";
+        System.out.println(i.toString());    }
+
+    @Test
+    public void TestClasse() {
+        Classe i;
+    }
 }
