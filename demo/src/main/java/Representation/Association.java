e5a696715d0168ff5ea382f1efd09d44da0adaf3
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2022-12-16 10:54:02 +0100

    implementation des relation

---
+++ demo/src/main/java/Representation/Association.java
@@ -0,0 +1,51 @@
+package Representation;
+
+public class Association extends Relation{
+    /**
+     * nombre de l'associatio de la classe src
+     */
+    private int nbSrc;
+    /**
+     * nombre de l'associatio de la classe cible
+     */
+    private int nbCible;
+    /**
+     * nom de l'attribut correspondant a cette association
+     */
+    private String nomAtt;
+
+    /**
+     * creer une association
+     * @param src
+     *      nom de la classe source
+     * @param cible
+     *      nom de la classe cible
+     * @param nsrc
+     *      nombre de l'associatio de la classe src
+     * @param ncible
+     *      nombre de l'associatio de la classe cible
+     * @param nom
+     *      nom de l'attribut correspondant a cette association
+     */
+    public Association(String src, String cible,int nsrc, int ncible, String nom) {
+        super(src, cible);
+        this.nbCible = ncible;
+        this.nbSrc = nsrc;
+        this.nomAtt = nom;
+    }
+
+    /**
+     *
+     * @return
+     *      un affichage de l'association
+     */
+    @Override
+    public String toString() {
+        if (this.nbCible==1){
+            return "\nla classe " +this.classeSrc + "contient un attribut " +this.nomAtt + "du type de la classe "+ this.classeCible;
+        }else {
+            return "\nla classe " +this.classeSrc + "contient un attribut " +this.nomAtt + "etant une liste d'objet de la classe  "+ this.classeCible;
+        }
+
+    }
+}
