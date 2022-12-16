40140779850c8f515c39b2ef2656dbc8871e36b7
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2022-12-16 12:34:31 +0100

    fin Sprint 1

--- demo/src/main/java/Representation/Main.java
+++
@@ -1,8 +0,0 @@
-package Representation;
-
-public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Modele m =new Modele("src/");
-        m.toString();
-    }
-}
