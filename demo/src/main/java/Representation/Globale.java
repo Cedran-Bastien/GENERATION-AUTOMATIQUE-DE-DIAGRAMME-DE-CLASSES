f23d8f55bfdb9df0871a9be3e2251e51a51d8e25
DIDIer5454 <menene12.akoo@gmail.com>
2022-12-16 10:37:51 +0100

    Globale

--- demo/src/main/java/Representation/Globale.java
+++ demo/src/main/java/Representation/Globale.java
@@ -7,18 +7,6 @@ public abstract class Globale {
     protected String nom;
     protected String acces;
     protected int modifier;
-    protected boolean visible;
-
-    public Globale(String chemin) throws ClassNotFoundException {
-        this.modifier= List.of(Modifier.toString(Class.forName(chemin).getModifiers()).split(" "));
-        this.nom=chemin;
-        this.visible=true;
-    }
-
-    public boolean isVisible() {
-        return visible;
-    }
-
     public String getNom() {
         return nom;
     }
