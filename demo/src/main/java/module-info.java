8be0006455ab726315ee0355b7af356fc2507fa5
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2023-01-03 22:25:14 +0100

    conception VueInstance

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module com.example.demo {
     requires junit;
-    requires org.testng;
-    requires org.junit.jupiter.api;
 //    requires org.junit.jupiter.api;
 //    requires org.testng;
     //requires javafx.controls;
